4973831ce474e03630c89b0b30086486a8ea3de5
s3m <s3m@archlinux.org>
2022-12-28 14:07:45 +0100

    minor updates

--- blocks.h
+++ blocks.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/        /*Command*/             /*Update Interval*/     /*Update Signal*/
-    {"  ",            "sb-brightness",        0,                      2},
+    {"",            "sb-brightness",        0,                      2},
     {"",            "sb-battery",           5,                      0},
     {"",            "sb-volume",            0,                      1},
-    {"",            "sb-clock",              60,                     0},
+    {"",            "sb-clock",             60,                     0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
