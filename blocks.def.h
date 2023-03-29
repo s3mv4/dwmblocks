64c1f04a719101aaefdf6fc9ab1a27c157cff625
s3m <s3m@archlinux.org>
2023-03-29 16:21:28 +0200

    added scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	/*Icon*/    /*Command*/	        /*Update Interval*/	    /*Update Signal*/
+	{"",        "sb-wifi",          5,		                0},
+	{"",        "sb-brightness",    5,		                2},
+	{"",        "sb-volume",        5,		                1},
+	{"",        "sb-battery",	    5,		                0},
+	{"",        "sb-date",	        5,		                0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
